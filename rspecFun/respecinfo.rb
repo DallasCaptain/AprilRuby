0fbc81955a9769d0ca950dd316bc7fd6baae08d1
Chris Thompson <chrisincr@gmail.com>
2020-04-15 15:51:01 -0500

    Wk2 Wednesday respec lecture

---
+++ rspecFun/respecinfo.rb
@@ -0,0 +1,27 @@
+RSpec.describe "Send Email" do
+    before(:each) do
+        @email = Email.create(form: "")
+    end
+
+    context "Email form is filled out" do
+        it "sends the email" do
+            @email.form ="filled out"
+            expect(@email.send).to eq(true)
+        end
+
+    end
+    context "Email form is not filled out" do
+        it "can't send the email" do
+            @email.form = ""
+            expect(@email.send).to eq(false)
+        end
+    end
+end
+RSpec.describe "Delete Email" do
+    
+
+
+
+
+    
+end
